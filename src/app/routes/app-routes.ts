8d100e21071f95758b49a651d24018236dca303b
Ivan Monzon <monzonyujraivanfranklin@gmail.com>
2023-07-19 21:37:32 -0400

    home, administration, sales-order modules added

---
+++ src/app/routes/app-routes.ts
@@ -0,0 +1,18 @@
+import {Routes} from '@angular/router';
+
+
+export const APP_ROUTES_CONFIG: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('../modules/home/home.module').then(module => module.HomeModule)},
+  {
+    path: 'salesOrder',
+    loadChildren: () => import('../modules/sales-order/sales-order.module').then(module => module.SalesOrderModule)},
+  {
+    path: 'administration',
+    loadChildren: () => import('../modules/administration/administration.module').then(module => module.AdministrationModule)
+  }
+];
